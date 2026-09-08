50c58e2a227d2eaf6960dcc458203dcaeda7946f
mach-port-t <fridakitten@pm.me>
2026-09-08 03:56:55 +0200

    fix: on ApplicationManagementViewController default to nil

--- Nyxian/UI/Settings/ApplicationManagement.swift
+++ Nyxian/UI/Settings/ApplicationManagement.swift
@@ -73,7 +73,7 @@ class ApplicationManagementViewController: UIThemedTableViewController, UITextFi
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let application: LDEApplicationObject = self.applications[indexPath.row]
         let cell: ProjectTableCell = self.tableView.dequeueReusableCell(withIdentifier: ProjectTableCell.reuseIdentifier) as! ProjectTableCell
-        cell.configure(displayName: application.localizedName, bundleIdentifier: application.bundleIdentifier, appIcon: application.icon ?? UIImage(named: "DefaultIcon"), showArrow: false)
+        cell.configure(displayName: application.localizedName, bundleIdentifier: application.bundleIdentifier, appIcon: application.icon, showArrow: false)
         return cell
     }
     
