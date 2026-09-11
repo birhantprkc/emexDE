019b3c2e279411c40bab69d703e1414b94898753
mach-port-t <fridakitten@pm.me>
2026-09-12 01:20:25 +0200

    refactor: nerfing kPEEntitlementFlagPlatformRoot ability to be a 2nd kPEEntitlementFlagProcessElevate

--- Nyxian/LindChain/ProcEnvironment/Surface/sys/cred/setuid.c
+++ Nyxian/LindChain/ProcEnvironment/Surface/sys/cred/setuid.c
@@ -58,8 +58,7 @@ void proc_set_sugid_if_applicable(ksurface_proc_t *proc,
 bool proc_is_privileged(ksurface_proc_t *proc)
 {
     /* Checking if process is entitled to elevate. */
-    if(entitlement_got_entitlement(proc_getentitlements(proc), kPEEntitlementFlagProcessElevate) ||
-       entitlement_got_entitlement(proc_getmaxentitlements(proc), kPEEntitlementFlagPlatform | kPEEntitlementFlagPlatformRoot))
+    if(entitlement_got_entitlement(proc_getentitlements(proc), kPEEntitlementFlagProcessElevate))
     {
         return true;
     }
