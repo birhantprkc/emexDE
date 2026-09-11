911661581a0fd34d75db5e04ad80c080ee397090
mach-port-t <fridakitten@pm.me>
2026-09-11 17:59:00 +0200

    refactor: use these softer system colors for diagnostics

--- Nyxian/UI/CodeEditor/CodeEditorCoordinator.swift
+++ Nyxian/UI/CodeEditor/CodeEditorCoordinator.swift
@@ -24,7 +24,6 @@ import UIKit
 import Runestone
 import MobileDevelopmentKit
 
-// MARK: - COORDINATOR
 class CodeEditorCoordinator: NSObject, TextViewDelegate {
     private(set) weak var parent: CodeEditorViewController?
     private var entries: [CFIndex:(NeoButton?,UIView?)] = [:]
@@ -36,10 +35,10 @@ class CodeEditorCoordinator: NSObject, TextViewDelegate {
     private(set) var debounce: LDEDebouncer?
     private(set) var diag: [MDKDiagnostic] = []
     private let vtkey: [CCDiagnosticLevel:(String,UIColor)] = [
-        .note: ("info.circle.fill", UIColor.blue.withAlphaComponent(0.3)),
-        .warning: ("exclamationmark.triangle.fill", UIColor.orange.withAlphaComponent(0.3)),
-        .error: ("xmark.octagon.fill", UIColor.red.withAlphaComponent(0.3)),
-        .fatal: ("xmark.octagon.fill", UIColor.red.withAlphaComponent(0.3))
+        .note: ("info.circle.fill", UIColor.systemBlue.withAlphaComponent(0.3)),
+        .warning: ("exclamationmark.triangle.fill", UIColor.systemOrange.withAlphaComponent(0.3)),
+        .error: ("xmark.octagon.fill", UIColor.systemRed.withAlphaComponent(0.3)),
+        .fatal: ("xmark.octagon.fill", UIColor.systemRed.withAlphaComponent(0.3))
     ]
     
     init(parent: CodeEditorViewController) {
