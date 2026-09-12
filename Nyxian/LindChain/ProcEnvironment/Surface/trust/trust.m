23978b3ee19cc4bd9a23fa96ac1c599d7110e04c
mach-port-t <fridakitten@pm.me>
2026-09-12 04:17:21 +0200

    security: removed old placeholder path

--- Nyxian/LindChain/ProcEnvironment/Surface/trust/trust.m
+++ Nyxian/LindChain/ProcEnvironment/Surface/trust/trust.m
@@ -442,10 +442,6 @@ ksurface_trust_identity_t *trust_identity_create_from_path(const char *path)
     /* daemon trustpath validation */
     trustDaemonEntry trustDaemonPath[] = {  /* those paths are immutable */
         {
-            .path = "/usr/libexec/bootstrapd",
-            .entitlementPreset = kPEEntitlementsNXT2PresetsDaemonBootstrap,
-        },
-        {
             .path = [NXBootstrap.shared.rootfsURL URLByAppendingPathComponent:@"boot/libexec/execd"].path.UTF8String,
             .entitlementPreset = kPEEntitlementsNXT2PresetsDaemonExec,
         },
